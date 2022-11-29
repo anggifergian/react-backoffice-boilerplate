ae9017dfaee4b053fab2a260ea47ef0641abdf45
koinworks <anggi.fergian@koinworks.com>
2022-11-29 15:14:14 +0700

    Setup routes react router v6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import './App.css';
+
+import Login from './pages/Login';
+
+import { privateRoutes } from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/*' element={<Navigate replace to='/login' />} />
+      <Route path='/login' element={<Login />} />
+      {privateRoutes.map(route1 => (
+        route1.submenu ? (
+          route1.submenu.map(route2 => (
+            route2.submenu ? (
+              route2.submenu.map(route3 => (
+                <Route
+                  key={route3.key}
+                  path={route3.path}
+                  element={route3.element}
+                />
+              ))
+            ) : (
+              <Route
+                key={route2.key}
+                path={route2.path}
+                element={route2.element}
+              />
+            )
+          ))
+        ) : (
+          <Route
+            key={route1.key}
+            path={route1.path}
+            element={route1.element}
+          />
+        )
+      ))}
+    </Routes>
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,116 @@
+import React from 'react';
+
+import AdminMenu from './pages/Menu';
+import AdminRole from './pages/Role';
+import AdminUser from './pages/User';
+
+import UserCore from './pages/UserCoreList';
+import UserCoreBank from './pages/UserCoreBank';
+import UserCoreDetail from './pages/UserCoreDetail';
+import UserCoreActivity from './pages/UserCoreActivity';
+
+import VcnList from './pages/VcnList';
+import VcnBalanceAdjustment from './pages/VcnBalanceAdjustment';
+
+import Neo from './pages/Neo';
+
+export const privateRoutes = [
+    {
+        key: 'admin',
+        label: 'Admin',
+        sidebar: true,
+        submenu: [
+            {
+                key: 'department',
+                label: 'Department',
+                sidebar: true,
+                submenu: [
+                    {
+                        key: 'neo',
+                        label: 'Neo',
+                        path: '/department/neo',
+                        element: <Neo />,
+                        sidebar: true,
+                    },
+                    {
+                        key: 'payment',
+                        label: 'Payment',
+                        path: '/department/payment',
+                        element: <AdminMenu />,
+                        sidebar: true,
+                    }
+                ]
+            },
+            {
+                key: 'adminMenu',
+                label: 'Menu',
+                path: '/admin/menu',
+                element: <AdminMenu />,
+                sidebar: true,
+            },
+            {
+                key: 'adminRole',
+                label: 'Role',
+                path: '/admin/role',
+                element: <AdminRole />,
+                sidebar: true,
+            },
+            {
+                key: 'adminUser',
+                label: 'User',
+                path: '/admin/user',
+                element: <AdminUser />,
+                sidebar: true,
+            }
+        ]
+    },
+    {
+        key: 'userCoreList',
+        label: 'User Core',
+        path: '/user-core/',
+        element: <UserCore />,
+        sidebar: true,
+    },
+    {
+        key: 'userCoreActivity',
+        label: 'Activity',
+        path: '/user-core/activity/:id',
+        element: <UserCoreActivity />,
+        sidebar: false,
+    },
+    {
+        key: 'userCoreDetail',
+        label: 'Detail',
+        path: '/user-core/identity/:id',
+        element: <UserCoreDetail />,
+        sidebar: false,
+    },
+    {
+        key: 'userCoreBank',
+        label: 'Bank',
+        path: '/user-core/bank/:id',
+        element: <UserCoreBank />,
+        sidebar: false,
+    },
+    {
+        key: 'vcn',
+        label: 'VCN',
+        sidebar: true,
+        submenu: [
+            {
+                key: 'vcnList',
+                label: 'VCN List',
+                path: '/vcn/vcn-menu',
+                element: <VcnList />,
+                sidebar: true,
+            },
+            {
+                key: 'vcnBalanceAdjustment',
+                label: 'VCN Balance Adjustment',
+                path: '/vcn/vcn-balance-adjustment',
+                element: <VcnBalanceAdjustment />,
+                sidebar: true,
+            }
+        ]
+    }
+]
